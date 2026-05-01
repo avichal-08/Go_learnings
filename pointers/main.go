6b51748fa0714f770ca670563b97ab8575499db4
Avichal Pandey <avichal2018lko@gmail.com>
2026-05-01 13:13:19 +0530

    pointers

---
+++ pointers/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 4
+	fmt.Println("add of a", &a)
+	square(&a)
+	fmt.Println("value of a after square", a)
+
+	ptr := &a
+	fmt.Println(*ptr)
+
+}
+
+func square(n *int) {
+	fmt.Println("add of n", *n)
+	*n *= *n
+}
+
+// if a func is returning pointer of a variable(ex: square returnin pointer to main) then
+// we compiler will use heap to store that returning memory for main func to access using pointer
+// because stack gets eliminated automatically after function life-time
